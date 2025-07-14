c91e8cce619db68f661d55fb736725691e14cb22
prachi <guddip515@gmail.com>
2025-07-14 15:55:41 +0530

    feat: add seeder script for sample quiz data initialization

---
+++ backend/seeder.js
@@ -0,0 +1,26 @@
+// import dotenv from "dotenv";
+// import connectDB from "./config/db.js";
+// import { Quiz } from "./models/quiz.model.js";
+
+// dotenv.config();
+// await connectDB();
+
+// const importData = async () => {
+//   const sampleQuiz = new Quiz({
+//     title: "Sample Quiz 1",
+//     description: "A sample quiz for testing 1",
+//     questions: [
+//       {
+//         questionText: "What is JavaScript?",
+//         options: ["Language", "Runtime", "Framework"],
+//         correctOption: 1,
+//       },
+//     ],
+//   });
+
+//   await sampleQuiz.save();
+//   console.log("Sample quiz saved!");
+//   process.exit();
+// };
+
+// importData();
